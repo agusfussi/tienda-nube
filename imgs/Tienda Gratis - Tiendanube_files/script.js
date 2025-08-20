09e663998fc815899bb7a157d645d774bfeefbd8
agusfussi <agusfussiwor@gmail.com>
2025-08-20 12:25:06 -0300

    mensaje de lo que hiciste

--- imgs/Tienda Gratis - Tiendanube_files/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.btn').forEach(btn => {
-  btn.addEventListener('click', e => {
-    e.preventDefault();
-    alert('Este botón simula la acción "Crear tienda gratis".');
-  });
-});
